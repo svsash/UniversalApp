431eba0e5c0a2dda7d742c1b7d426bba4d7f13f5
svsash <pyavchik.aleksan@gmail.com>
2013-09-05 20:11:49 +0300

    create project

---
+++ UniversalApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  UniversalApp
+//
+//  Created by Machine Test on 05.09.13.
+//  Copyright (c) 2013 AleksandrP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ UniversalApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UniversalApp
+//
+//  Created by Machine Test on 05.09.13.
+//  Copyright (c) 2013 AleksandrP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
